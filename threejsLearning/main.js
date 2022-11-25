e5ba91d01b5f63f404b2da753b57db6fe50c3e04
himTresor1 <amizeropeace@gmail.com>
2022-11-25 19:22:09 +0200

    Added a box mesh to my scene

--- threejsLearning/main.js
+++ threejsLearning/main.js
@@ -2,12 +2,29 @@ import  * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
 
       const scene = new THREE.Scene();
       const camera = new THREE.PerspectiveCamera(75 , innerWidth/innerHeight,0.1, 1000)
-      const render =  new THREE.WebGLRenderer()
+      const renderer =  new THREE.WebGLRenderer()
        console.log(scene); 
        console.log(camera);
-       console.log(render);
-        render.setSize(innerWidth, innerHeight)
-        document.body.appendChild(render.domElement);
+       console.log(renderer);
+        renderer.setSize(innerWidth, innerHeight)
+        document.body.appendChild(renderer.domElement);
 
 
-         const boxGe
+         const boxGeometry = new THREE.BoxGeometry(1 , 1, 1);
+
+          const material = new THREE.MeshBasicMaterial({color: 0x00FF00})
+         console.log(boxGeometry)
+         console.log(material)
+
+
+         const mesh = new THREE.Mesh(boxGeometry, material)
+          console.log(mesh)
+
+
+           scene.add(mesh)
+           camera.position.z =5   
+
+
+            renderer.render(scene, camera)
+
+
