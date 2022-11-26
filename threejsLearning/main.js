4b275b396358513f372b1eab95b6f835a7044739
himTresor1 <amizeropeace@gmail.com>
2022-11-26 10:27:34 +0200

    Morning Push

--- threejsLearning/main.js
+++ threejsLearning/main.js
@@ -3,28 +3,54 @@ import  * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
       const scene = new THREE.Scene();
       const camera = new THREE.PerspectiveCamera(75 , innerWidth/innerHeight,0.1, 1000)
       const renderer =  new THREE.WebGLRenderer()
-       console.log(scene); 
-       console.log(camera);
-       console.log(renderer);
+      
         renderer.setSize(innerWidth, innerHeight)
+        renderer.setPixelRatio(devicePixelRatio)
         document.body.appendChild(renderer.domElement);
 
 
-         const boxGeometry = new THREE.BoxGeometry(1 , 1, 1);
-
-          const material = new THREE.MeshBasicMaterial({color: 0x00FF00})
-         console.log(boxGeometry)
-         console.log(material)
-
-
-         const mesh = new THREE.Mesh(boxGeometry, material)
-          console.log(mesh)
-
-
-           scene.add(mesh)
+        
            camera.position.z =5   
 
+           const planeGeometry = new THREE.PlaneGeometry(10 , 10 , 10 ,10 );  
+           const planeMaterial = new THREE.MeshPhongMaterial({
+            color : 0xFF0000 , 
+            side : THREE.DoubleSide,
+            flatShading : THREE.FlatShading
 
-            renderer.render(scene, camera)
+          });  
+           const  planeMesh = new THREE.Mesh(planeGeometry, planeMaterial)
+           scene.add(planeMesh)
+
+           const light = new THREE.DirectionalLight(
+            0xFFFFFF, 1
+           )
+           light.position.set(0,0,1)
+            scene.add(light )
+            console.log(planeMesh.geometry.attributes.position.array);
+                 const {array} = planeMesh.geometry.attributes.position;
+            for(let i=0 ;  i  <array.length ; i+=3 ){
+               const x= array[i]
+               const y= array[i+1]
+               const z= array[i+2] 
+              // console.log(array[i]); 
+          
+               array[ i+2 ] =  z+ Math.random()  
+                
+                      
+            }
+        
+
+             function animate(){
+               requestAnimationFrame(animate)
+               renderer.render(scene, camera)
+              
+
+              //  planeMesh.rotation.x +=0.01
+              //  planeMesh.rotation.y +=0.01 
+             }
+ 
+
+            animate();
 
 
