0605d6d73498603593b60e62b0d88324c338ec25
himTresor1 <amizeropeace@gmail.com>
2022-11-27 20:15:10 +0200

    Added OrbitControls

--- threejsLearning/main.js
+++ threejsLearning/main.js
@@ -1,4 +1,5 @@
 import  * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
+import {OrbitControls} from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js'
 import  * as dat from 'dat.gui'
 
 
@@ -14,11 +15,11 @@ import  * as dat from 'dat.gui'
  }
 
   
-gui.add(world.plane, 'width' , 1 ,  20).onChange(generatePlane)
+gui.add(world.plane, 'width' , 1 ,  50).onChange(generatePlane)
 
-gui.add(world.plane,'height', 1,20 ).onChange(generatePlane)
- gui.add(world.plane ,'widthSegments' , 1,20 ).onChange(generatePlane)
- gui.add(world.plane ,'heightSegments' , 1,20 ).onChange(generatePlane)
+gui.add(world.plane,'height', 1,50 ).onChange(generatePlane)
+ gui.add(world.plane ,'widthSegments' , 1,50 ).onChange(generatePlane)
+ gui.add(world.plane ,'heightSegments' , 1,50 ).onChange(generatePlane)
 
    function generatePlane  (){
     planeMesh.geometry.dispose()
@@ -33,39 +34,24 @@ gui.add(world.plane,'height', 1,20 ).onChange(generatePlane)
        const x= array[i]
        const y= array[i+1]
        const z= array[i+2] 
-      // console.log(array[i]); 
-  
-       array[ i+2 ] =  z+ Math.random()  
-        
-              
-    }
-  
-  
-    
-  }
-  
-  
-//   onChange( () => { ===
-//   console.log(world.plane.width)
-// })
-
-
-
-      const scene = new THREE.Scene();
+     array[ i+2 ] =  z+ Math.random()  
+}}
+const scene = new THREE.Scene();
       const camera = new THREE.PerspectiveCamera(75 , innerWidth/innerHeight,0.1, 1000)
       const renderer =  new THREE.WebGLRenderer()
+
       
         renderer.setSize(innerWidth, innerHeight)
         renderer.setPixelRatio(devicePixelRatio)
         document.body.appendChild(renderer.domElement);
-
+          new OrbitControls(camera,renderer.domElement)
 
         
            camera.position.z =5   
 
            const planeGeometry = new THREE.PlaneGeometry(10 , 10 , 10 ,10 );  
            const planeMaterial = new THREE.MeshPhongMaterial({
-            color : 0xFF0000 , 
+            color : 0xFF0000, 
             side : THREE.DoubleSide,
             flatShading : THREE.FlatShading
 
@@ -74,10 +60,17 @@ gui.add(world.plane,'height', 1,20 ).onChange(generatePlane)
            scene.add(planeMesh)
 
            const light = new THREE.DirectionalLight(
-            0xFFFFFF, 1
-           )
+            0xFFFFFF, 1)
+
            light.position.set(0,0,1)
             scene.add(light )
+
+            // const backLight = new THREE.DirectionalLight(0xFFFFFF, 1 )
+            // light.position.set(0,0,-1)
+            //   scene.add(backLight )
+               const backLight= new THREE.DirectionalLight(0xFFFFFF,10)
+               light.position.set(0,0,-1)
+               scene.add(backLight)
             console.log(planeMesh.geometry.attributes.position.array);
                  const {array} = planeMesh.geometry.attributes.position
             for(let i=0 ;  i  <array.length ; i+=3 ){
